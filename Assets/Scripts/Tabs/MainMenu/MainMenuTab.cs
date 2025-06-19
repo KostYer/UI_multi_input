002799e146440b26617aa407d39d9e8fe997feb1
KostYer <KostYer@yahoo.com>
2025-06-19 12:53:11 +0300

    [MOD] settings

--- Assets/Scripts/Tabs/MainMenu/MainMenuTab.cs
+++ Assets/Scripts/Tabs/MainMenu/MainMenuTab.cs
@@ -1,6 +1,40 @@
+using System.Collections;
+using DG.Tweening;
 using Screens;
+using UnityEngine;
+using UnityEngine.UI;
 
 public class MainMenuTab : ScreenTab
 {
+  
+   [SerializeField] private CanvasGroup _rawImage;
+   [SerializeField] private UiVfxSettingsSO _settingsSO;
+
+   private Tween _fadeTween;
+
+   public void FadeTo(float targetAlpha)
+   {
+      if (_rawImage == null) return;
+
+      _fadeTween?.Kill();
+
+      _fadeTween = _rawImage.DOFade(targetAlpha, _settingsSO.FadeDuration)
+         .SetEase(Ease.InOutSine)
+         .SetUpdate(true);  
+   }
+
    
+ 
+   public override void Show()
+   {
+      _rawImage.alpha = 0;
+      base.Show();
+      FadeTo(1);
+   }
+
+   public override void Hide()
+   {
+      base.Hide();
+      _rawImage.alpha = 0;
+   }
 }
--- Assets/Scripts/UiVfxSettings/UiVfxSettingsSO.cs
+++ Assets/Scripts/UiVfxSettings/UiVfxSettingsSO.cs
@@ -6,7 +6,9 @@ public class UiVfxSettingsSO : ScriptableObject
 {
     [SerializeField] private float _rotationSpeed;
     [SerializeField] private float _transitionDuration;
+    [SerializeField] private float _fadeDuration = .2f;
     
     public float RotationSpeed => _rotationSpeed;
     public float TransitionDuration => _transitionDuration;
+    public float FadeDuration => _fadeDuration;
 }
